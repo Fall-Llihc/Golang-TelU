4c8125f1c31352dbdc2dceccfa7452a14ec277d7
Llihc <falih.qwerty13@gmail.com>
2023-12-24 12:06:33 +0700

    Update Biner.go

--- Week14/Biner.go
+++ Week14/Biner.go
@@ -3,15 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	var x, i, bin int
+	// var x, i, bin int       			"DENGAN PERKALIAN & TANPA IF"
+
+	// fmt.Scan(&x)
+
+	// i = 1
+	// for(x > 0){
+	// 	bin = bin + (x%2 * i)
+	// 	i *= 10
+	// 	x /= 2
+	// }
+	// fmt.Println(bin) 				"TINGGA DI CONVERT KE STRING"
+
+	var x int
+	var y string
+
+	y = ""								//LANGSUNG JADI STRING
 
 	fmt.Scan(&x)
-
-	i = 1
-	for(x > 0){
-		bin = bin + (x%2 * i)
-		i *= 10
+	for x > 0 {
+		if x%2 == 0 {
+			y = "0" + y
+		} else {
+			y = "1" + y
+		}
 		x /= 2
 	}
-	fmt.Println(bin) 
+	fmt.Println(y)
 }
